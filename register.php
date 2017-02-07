175eff75bb78460491e39df0af3f617b808bc2db
vasanthsiva <vasanthsiva225@gmail.com>
2017-02-07 23:09:39 +0530

    siva added file

---
+++ register.php
@@ -0,0 +1,34 @@
+<?php
+	require('connect.php');
+    // If the values are posted, insert them into the database.
+    if (isset($_POST['username']) && isset($_POST['password'])){
+        $username = $_POST['username'];
+	$email = $_POST['email'];
+        $password = $_POST['password'];
+ 
+        $query = "INSERT INTO `user` (username, password, email) VALUES ('$username', '$password', '$email')";
+        $result = mysqli_query($connection, $query);
+        if($result){
+            $smsg = "User Created Successfully.";
+        }else{
+            $fmsg ="User Registration Failed";
+        }
+    }
+    ?>
+<html>
+<div class="container">
+      <form class="form-signin" method="POST">
+        <h2 class="form-signin-heading">Please Register</h2>
+        <div class="input-group">
+	  <span class="input-group-addon" id="basic-addon1">@</span>
+	  <input type="text" name="username" class="form-control" placeholder="Username" required>
+	</div>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
+        <a class="btn btn-lg btn-primary btn-block" href="login.php">Login</a>
+      </form>
+</div>
+</html>
